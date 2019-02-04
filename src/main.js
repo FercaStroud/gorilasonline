792592e11d361ddf4ddf1460cd09ca62cef01e60
Fernando Cárdenas <cardenas314_@outlook.com>
2019-02-03 22:53:23 -0600

    dogma

--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-
+/*
+import "@/assets/css/reset.css"
+import "@/assets/css/plugins.css"
+import "@/assets/css/style.css"
 import "@/assets/scss/styles.scss"
+
+import jq from "@/assets/js/jquery.min.js"
+import plugins from "@/assets/js/plugins.js"
+import core from "@/assets/js/core.js"
+import scripts from "@/assets/js/scripts.js"
+
+Vue.use(jq)
+Vue.use(plugins)
+Vue.use(core)
+Vue.use(scripts)*/
 
 Vue.config.productionTip = false
 
