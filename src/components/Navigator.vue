d185541454c2f9fa3f616d4acd9e9219c78ee5be
Fernando Cárdenas <cardenas314_@outlook.com>
2019-04-23 22:14:32 -0500

    asdf

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,27 +1,27 @@
 <template>
     <div>
-        <vs-navbar v-model="activeItem" text-color="#58595b" active-text-color="#d5643c">
-            <div slot="title">
-                <vs-navbar-title>
-                    <img style="max-height: 45px" src="../assets/images/n-l-horizontal.png">
-                </vs-navbar-title>
-            </div>
+        <vs-navbar v-model="activeItem" text-color="#58595b" active-text-color="#fe3f16">
+                <div slot="title">
+                    <vs-navbar-title>
+                        <img style="max-height: 45px; margin:10px" src="../assets/images/n-l-horizontal.png">
+                    </vs-navbar-title>
+                </div>
 
-            <vs-navbar-item index="0">
-                <a href="#">Nosotros</a>
-            </vs-navbar-item>
-            <vs-navbar-item index="1">
-                <a href="#">Productos</a>
-            </vs-navbar-item>
-            <vs-navbar-item index="2">
-                <a href="#">Portafolio</a>
-            </vs-navbar-item>
-            <vs-navbar-item index="3">
-                <a href="#">Tecnologīa</a>
-            </vs-navbar-item>
-            <vs-navbar-item index="4">
-                <a href="#">Contacto</a>
-            </vs-navbar-item>
+                <vs-navbar-item index="0">
+                    <a href="#">Nosotros</a>
+                </vs-navbar-item>
+                <vs-navbar-item index="1">
+                    <a href="#">Productos</a>
+                </vs-navbar-item>
+                <vs-navbar-item index="2">
+                    <a href="#">Portafolio</a>
+                </vs-navbar-item>
+                <vs-navbar-item index="3">
+                    <a href="#">Tecnologīa</a>
+                </vs-navbar-item>
+                <vs-navbar-item index="4">
+                    <a href="#">Contacto</a>
+                </vs-navbar-item>
         </vs-navbar>
     </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="home">
-
-  </div>
+    <div class="home">
+        <div class="">
+            <img src="../assets/images/">
+        </div>
+    </div>
 </template>
 
 <script>
 
-export default {
-  name: 'home',
-  components: {
-
-  }
-}
+    export default {
+        name: 'home',
+        components: {}
+    }
 </script>
